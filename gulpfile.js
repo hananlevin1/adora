e9271b335807a1a55eb5dbf558d0796d8989a7ae
evilkrow <krowevil@gmail.com>
2015-08-03 10:06:51 +0200

    added readme

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,25 @@ var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
     sourcemaps = require('gulp-sourcemaps'),
     autoprefixer = require('autoprefixer'),
-    lost = require('lost');
+    lost = require('lost'),
+    modularscale = require('postcss-modular-scale');
 
 var paths = {
-  cssSource: 'src/css/',
-  cssDestination: 'assets/css/'
+    cssSource: 'src/css/',
+    cssDestination: 'assets/css/'
 };
 
 gulp.task('styles', function() {
-  return gulp.src(paths.cssSource + '**/*.css')
-    .pipe(sourcemaps.init())
-    .pipe(postcss([
-      lost(),
-      autoprefixer()
-    ]))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(paths.cssDestination));
+    return gulp.src(paths.cssSource + '**/*.css')
+	.pipe(sourcemaps.init())
+	.pipe(postcss([
+	    lost(),
+	    autoprefixer(),
+	    modularscale()
+	    
+	]))
+	.pipe(sourcemaps.write('./'))
+	.pipe(gulp.dest(paths.cssDestination));
 });
 
 gulp.watch(paths.cssSource + '**/*.css', ['styles']);
